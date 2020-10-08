1dce84ac584c425ce729d70b923b0f8db35ab290
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-10-08 15:49:07 -0600

    up

--- courseware/prepassign/module3/lesson14-15_recipe_app/main.js
+++ courseware/prepassign/module3/lesson14-15_recipe_app/main.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.connect(
   "mongodb+srv://axat:axat123@cluster0.y9onm.mongodb.net/recipe_db?retryWrites=true&w=majority",
-  {useNewUrlParser: true}
+  {useNewUrlParser: true, useUnifiedTopology: true}
 );
 const db = mongoose.connection;
 
