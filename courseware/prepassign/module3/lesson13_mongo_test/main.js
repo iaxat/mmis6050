8a1744f3930a8519322c36ae2b438ea1b3953700
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-10-08 03:25:21 -0600

    up

--- courseware/prepassign/module3/lesson13_mongo_test/main.js
+++ courseware/prepassign/module3/lesson13_mongo_test/main.js
@@ -1,6 +1,6 @@
 const MongoDB = require("mongodb").MongoClient,    
   dbURL = "mongodb+srv://axat:axat123@cluster0.y9onm.mongodb.net/recipe_db?retryWrites=true&w=majority",
-  dbName = "recipe_db";
+  dbName = "cluster0";
 
 MongoDB.connect(dbURL, (error, client) => {        
   if (error) throw error;
