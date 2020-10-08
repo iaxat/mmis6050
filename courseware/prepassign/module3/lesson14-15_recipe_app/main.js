92a74a62686dc738e8486143e90681eb03c5b37e
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-10-08 16:05:56 -0600

    up

--- courseware/prepassign/module3/lesson14-15_recipe_app/main.js
+++ courseware/prepassign/module3/lesson14-15_recipe_app/main.js
@@ -25,3 +25,24 @@ const Subscriber = mongoose.Schema({
 })
 
 const Subscriber = mongoose.model(Subscriber, subscriberSchema)
+
+var subscriber1 = new Subscriber({
+  name: "Jon Wexler",
+  email: "jon@jonwexler.com"
+});                                              1
+
+subscriber1.save((error, savedDocument) => {     2
+  if (error) console.log(error);                 3
+  console.log(savedDocument);                    4
+});
+
+Subscriber.create(
+  {
+    name: "Jon Wexler",
+    email: "jon@jonwexler.com"
+  },
+  function (error, savedDocument) {              5
+    if (error) console.log(error);
+    console.log(savedDocument);
+  }
+);
