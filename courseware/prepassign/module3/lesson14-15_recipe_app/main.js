849b42de3cd03336fe453454097fcadb6536e726
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-10-08 13:42:55 -0600

    up

--- courseware/prepassign/module3/lesson14-15_recipe_app/main.js
+++ courseware/prepassign/module3/lesson14-15_recipe_app/main.js
@@ -1,42 +1,11 @@
-"use strict";
-// Import needed modules
-const express = require("express"),
-  app = express(),
-  errorController = require("./controllers/errorController"),
-  homeController = require("./controllers/homeController"),
-  layouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://axat:axat123@cluster0.y9onm.mongodb.net/recipe_db?retryWrites=true&w=majority",
+  {useNewUrlParser: true}
+);
+const db = mongoose.connection;
 
-// Set application variables
-app.set("port", process.env.PORT || 3000);
-app.set("view engine", "ejs");
-
-// Set up application middleware
-app.use(express.static("public"));
-app.use(layouts);
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(homeController.logRequestPaths);
-
-//  Routes for testing
-app.get("/name", homeController.respondWithName);
-app.get("/items/:vegetable", homeController.sendReqParam);
-
-
-// Routes for home page, courses page, and contact page
-app.get("/", homeController.index);
-app.get("/courses", homeController.showCourses);
-app.get("/contact", homeController.showContact);
-app.post("/contact", homeController.postContact);
-
-
-// Set up error handling middleware at the end
-// These should only be applied if no other routes apply
-app.use(errorController.logErrors);
-app.use(errorController.respondNoResourceFound);
-app.use(errorController.respondInternalError);
-
-
-// Launch the server
-app.listen(app.get("port"), () => {
-  console.log(`Server running at mongodb+srv://axat:axat123@cluster0.y9onm.mongodb.net/recipe_db?retryWrites=true&w=majority:${app.get("port")}`);
+db.once("open", () => {
+  console.log("Successfully connected to MongoDB using Mongoose!");
 });
+
