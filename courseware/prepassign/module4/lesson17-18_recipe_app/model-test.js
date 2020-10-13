c9e88fc40b271ca52b28eddff45f7dd6e1cd647b
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2020-10-13 05:00:34 -0600

    up

--- courseware/prepassign/module4/lesson17-18_recipe_app/model-test.js
+++ courseware/prepassign/module4/lesson17-18_recipe_app/model-test.js
@@ -1,19 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"),
 
-const subscriberSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-    unique: true
-  },
-  zipCode: {
-    type: Number,
-    min: [10000, "Zip code too short"],
-    max: 99999
-  }
-});
+  Subscriber = require("./models/subscriber");
+
+mongoose.connect(
+  "mongodb://localhost:27017/recipe_db",
+  {useNewUrlParser: true}
+);
+
+mongoose.Promise = global.Promise;
