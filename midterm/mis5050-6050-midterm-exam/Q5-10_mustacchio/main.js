228b28e0e27e8e37a741d06bba4bb4619b5f704c
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-09 11:05:07 -0700

    up

---
+++ midterm/mis5050-6050-midterm-exam/Q5-10_mustacchio/main.js
@@ -0,0 +1,10 @@
+// The main.js file section g
+
+"use strict";
+
+const express = require("express"),
+    app = express(),
+    router = express.Router(),
+    layouts = require("express-ejs-layouts")
+    ;
+    
