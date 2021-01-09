5cc8502c227c9cf9b9e0d7ea59e7e6a21e418d6f
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-09 16:28:27 -0700

    up

--- midterm/mis5050-6050-midterm-exam/Q5-10_mustacchio/models/mustache.js
+++ midterm/mis5050-6050-midterm-exam/Q5-10_mustacchio/models/mustache.js
@@ -4,5 +4,20 @@ const mongoose = require("mongoose"),
   { Schema } = mongoose;
 
 var mustacheSchema = new Schema(
-    
+    {
+        title : {
+            type: String,
+            required: true
+        },
+
+        imageUrl: {
+            type: String,
+            required: true
+        },
+
+        description: {
+            type: String,
+            required: true
+        }
+    }
 )
