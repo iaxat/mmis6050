76e91efcf2def783f13d53c74f5b81061e117aae
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-10 14:59:11 -0700

    up

--- midterm/mis5050-6050-midterm-exam/Q3/Q3.js
+++ midterm/mis5050-6050-midterm-exam/Q3/Q3.js
@@ -6,32 +6,42 @@ var friends = [
     {
         "firstName" : "Chandler",
         "lastName" : "Whitley",
-        "birthDate" : new Date(),
-        "favoriteFoods" : []
+        "birthDate" : new Date(1995,02,02),
+        "favoriteFoods" : ['taco','burger']
     },
     {
         "firstName" : "Acea",
         "lastName" : "Winston",
-        "birthDate" : new Date(),
-        "favoriteFoods" : []
+        "birthDate" : new Date(1995,02,08),
+        "favoriteFoods" : ['pizza','pie']
     },
     {
         "firstName" : "Bryan",
         "lastName" : "Lankford",
-        "birthDate" : new Date(),
-        "favoriteFoods" : []
+        "birthDate" : new Date(1995,02,07),
+        "favoriteFoods" : ['burrito','burger']
     },
     {
         "firstName" : "Jake",
         "lastName" : "Howell",
-        "birthDate" : new Date(),
-        "favoriteFoods" : []
+        "birthDate" : new Date(1995,02,01),
+        "favoriteFoods" : ['noodles','taco']
     }
 ]
 
 
 // Section 'b'
-// The function to accept the array of functions
+// The function to accept the array of objects
 
+const friendFunction = (friends) => {
+    console.log(friends);
+}
 
-friendFunction.apply(window,{friends});
+friendFunction(friends)
+
+// const fuc1=(a,b)=>{
+//     return a+b;
+// }
+// console.log(fuc1)
+
+// friendFunction.apply(window,{friends});
