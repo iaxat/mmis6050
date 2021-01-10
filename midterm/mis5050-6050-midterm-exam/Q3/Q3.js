e7c9a28689a414b6bea6ad3cd44caf3f745e8f6c
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-10 15:18:18 -0700

    up

--- midterm/mis5050-6050-midterm-exam/Q3/Q3.js
+++ midterm/mis5050-6050-midterm-exam/Q3/Q3.js
@@ -6,19 +6,19 @@ var friends = [
     {
         "firstName" : "Chandler",
         "lastName" : "Whitley",
-        "birthDate" : new Date(1995,02,02),
+        "birthDate" : new Date(1997,02,02),
         "favoriteFoods" : ['taco','burger']
     },
     {
         "firstName" : "Acea",
         "lastName" : "Winston",
-        "birthDate" : new Date(1995,02,08),
+        "birthDate" : new Date(1990,02,08),
         "favoriteFoods" : ['pizza','pie']
     },
     {
         "firstName" : "Bryan",
         "lastName" : "Lankford",
-        "birthDate" : new Date(1995,02,07),
+        "birthDate" : new Date(1993,02,07),
         "favoriteFoods" : ['burrito','burger']
     },
     {
@@ -29,19 +29,12 @@ var friends = [
     }
 ]
 
-
 // Section 'b'
 // The function to accept the array of objects
 
-const friendFunction = (friends) => {
-    console.log(friends);
-}
+// to find the age we can approach with 2 methods
+// since JS stores date till milliseconds, we can just use the 
+// normal new date function and subtract 
+// but this will make it in milliseconds
 
-friendFunction(friends)
-
-// const fuc1=(a,b)=>{
-//     return a+b;
-// }
-// console.log(fuc1)
-
-// friendFunction.apply(window,{friends});
+friends.forEach((friend)=>console.log(friend.firstName,friend.lastName,'is',Math.round((new Date()-friend.birthDate)/(86400000*365)),'years old and likes the following foods:',friend.favoriteFoods));
