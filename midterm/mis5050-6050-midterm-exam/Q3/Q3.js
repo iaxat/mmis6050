bbccd1ee5f46927d5e4f558b28a49db67bb526c2
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-10 15:47:53 -0700

    up

--- midterm/mis5050-6050-midterm-exam/Q3/Q3.js
+++ midterm/mis5050-6050-midterm-exam/Q3/Q3.js
@@ -46,3 +46,15 @@ friends.forEach((friend)=>console.log(friend.firstName,friend.lastName,'is',Math
 // Section 'c'
 // taking each 
 // var inDate = prompt('Enter date yyyy,mm,dd');
+// friends.forEach((friend,inDate)=>console.log(inDate));
+
+const functDate(friends, date)=>{
+    console.log();
+}
+
+
+const funcDate(date, friends)=>{
+    if (Math.round((date)-friend.birthDate)/(86400000*365)) )
+    
+}
+console.log(funcDate("gfaghxfa",friends))
