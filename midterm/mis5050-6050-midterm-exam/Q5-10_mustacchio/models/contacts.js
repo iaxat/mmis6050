20967abfa66cf3240c126fd1bb042db982557b6a
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-12 04:40:56 -0700

    up

--- midterm/mis5050-6050-midterm-exam/Q5-10_mustacchio/models/contacts.js
+++ midterm/mis5050-6050-midterm-exam/Q5-10_mustacchio/models/contacts.js
@@ -1,2 +1,45 @@
 // Question 8
 
+// Question 7 - Schema
+
+"use strict";
+
+const mongoose = require("mongoose"),
+  { Schema } = mongoose;
+
+var blogSchema = new Schema(
+    {
+        title : {
+            type: String,
+            required: true,
+            max: 30
+        },
+
+        summary: {
+            type: String,
+            required: true,
+            max: 250
+        },
+
+        content: {
+            type: String,
+            required: true
+        },
+        
+        imageUrl: {
+            type: String,
+            required: true,
+            allowedFormats: ["jpg","png"]
+
+        },
+
+        datePosted: {
+            type: Date,
+            required: true
+        }
+
+    }
+);
+
+module.exports = mongoose.model("blogPost", blogSchema);
+
