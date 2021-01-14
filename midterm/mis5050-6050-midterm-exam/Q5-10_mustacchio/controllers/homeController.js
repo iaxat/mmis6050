4eb484310d971de8f0a141b232c1e0497e9c683c
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-01-14 01:00:57 -0700

    up

--- midterm/mis5050-6050-midterm-exam/Q5-10_mustacchio/controllers/homeController.js
+++ midterm/mis5050-6050-midterm-exam/Q5-10_mustacchio/controllers/homeController.js
@@ -8,5 +8,18 @@ module.exports = {
   },
   about: (req, res) => {
     res.render("about");
-  }
+  },
+  logRequestPaths: (req, res, next) => {
+    // console.log(req.method)
+    let today =  new Date();
+    let str=today+' '+req.method+' '+ 'request made to: '+req.url;
+    console.log(today,` `,req.method,` request made to: ${req.url}`);
+    fs.writeFile('log.txt', str, function(err) {
+      if (err) {
+         return console.error(err);
+      }else{
+        next();
+      }
+  })
+}
 };
