4fe0eb1ca98c3fed19e843973631d37720797e81
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-02-10 00:02:53 -0700

    up

--- module5.2/prep/lesson24_recipe_app/models/user.js
+++ module5.2/prep/lesson24_recipe_app/models/user.js
@@ -64,24 +64,24 @@ userSchema.pre("save", function(next) {
   }
 });
 
-userSchema.pre("save", function(next) {
-  let user = this;
-  bcrypt
-    .hash(user.password, 10)
-    .then(hash => {
-      user.password = hash;
-      next();
-    })
-    .catch(error => {
-      console.log(`Error in hashing password: ${error.message}`);
-      next(error);
-    });
-});
+// userSchema.pre("save", function(next) {
+//   let user = this;
+//   bcrypt
+//     .hash(user.password, 10)
+//     .then(hash => {
+//       user.password = hash;
+//       next();
+//     })
+//     .catch(error => {
+//       console.log(`Error in hashing password: ${error.message}`);
+//       next(error);
+//     });
+// });
 
-userSchema.methods.passwordComparison = function(inputPassword) {
-  let user = this;
-  return bcrypt.compare(inputPassword, user.password);
-};
+// userSchema.methods.passwordComparison = function(inputPassword) {
+//   let user = this;
+//   return bcrypt.compare(inputPassword, user.password);
+// };
 
 
 
