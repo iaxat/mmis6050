57e07fa5d5851af797a648889e183f54bb268693
iaxat <55215422+iaxat@users.noreply.github.com>
2021-03-09 22:36:33 -0700

    up

---
+++ module7.1/prep/lesson30-31_recipe_app(1)/lesson30-31_recipe_app/routes/homeRoutes.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const router = require("express").Router(),
+  homeController = require("../controllers/homeController");
+
+router.get("/", homeController.index);
+router.get("/contact", homeController.contact);
+router.get("/chat", homeController.chat);
+
+module.exports = router;
