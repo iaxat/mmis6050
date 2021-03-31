2df0e6121b699c56ba6318ebc1ec585029958064
Kumar Akshat <55215422+iaxat@users.noreply.github.com>
2021-03-31 03:36:21 -0600

    up

--- module8.1/prep/lesson34-35_recipe_app/main.js
+++ module8.1/prep/lesson34-35_recipe_app/main.js
@@ -1,33 +1,36 @@
 "use strict";
 
 const express = require("express"),
-  app = express(),
-  router = require("./routes/index"),
-  layouts = require("express-ejs-layouts"),
-  mongoose = require("mongoose"),
-  methodOverride = require("method-override"),
-  expressSession = require("express-session"),
-  cookieParser = require("cookie-parser"),
-  connectFlash = require("connect-flash"),
-  expressValidator = require("express-validator"),
-  passport = require("passport"),
-  errorController = require("./controllers/errorController"),
-  homeController = require("./controllers/homeController"),
-  subscribersController = require("./controllers/subscribersController"),
-  usersController = require("./controllers/usersController"),
-  coursesController = require("./controllers/coursesController"),
-  User = require("./models/user");
-  const morgan = require("morgan");
+	app = express(),
+	router = require("./routes/index"),
+	layouts = require("express-ejs-layouts"),
+	mongoose = require("mongoose"),
+	methodOverride = require("method-override"),
+	expressSession = require("express-session"),
+	cookieParser = require("cookie-parser"),
+	connectFlash = require("connect-flash"),
+	expressValidator = require("express-validator"),
+	passport = require("passport"),
+	errorController = require("./controllers/errorController"),
+	homeController = require("./controllers/homeController"),
+	subscribersController = require("./controllers/subscribersController"),
+	usersController = require("./controllers/usersController"),
+	coursesController = require("./controllers/coursesController");
+
+const User =require("./models/user");
+  
+
+const morgan = require("morgan");
 
 //mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.MONGODB_URI,
-  {useNewUrlParser: true});
+	{useNewUrlParser: true});
 
 const db = mongoose.connection;
 
 db.once("open", () => {
-  console.log("Successfully connected to MongoDB using Mongoose!");
+	console.log("Successfully connected to MongoDB using Mongoose!");
 });
 
 app.set("port", process.env.PORT || 3000);
@@ -37,28 +40,28 @@ app.set("token", process.env.TOKEN || "recipeT0k3n");
 app.use(express.static("public"));
 app.use(layouts);
 app.use(
-  express.urlencoded({
-    extended: false
-  })
+	express.urlencoded({
+		extended: false
+	})
 );
 
 app.use(
-  methodOverride("_method", {
-    methods: ["POST", "GET"]
-  })
+	methodOverride("_method", {
+		methods: ["POST", "GET"]
+	})
 );
 
 app.use(express.json());
 app.use(cookieParser("secret_passcode"));
 app.use(
-  expressSession({
-    secret: "secret_passcode",
-    cookie: {
-      maxAge: 4000000
-    },
-    resave: false,
-    saveUninitialized: false
-  })
+	expressSession({
+		secret: "secret_passcode",
+		cookie: {
+			maxAge: 4000000
+		},
+		resave: false,
+		saveUninitialized: false
+	})
 );
 
 app.use(passport.initialize());
@@ -69,16 +72,16 @@ passport.deserializeUser(User.deserializeUser());
 app.use(connectFlash());
 
 app.use((req, res, next) => {
-  res.locals.loggedIn = req.isAuthenticated();
-  res.locals.currentUser = req.user;
-  res.locals.flashMessages = req.flash();
-  next();
+	res.locals.loggedIn = req.isAuthenticated();
+	res.locals.currentUser = req.user;
+	res.locals.flashMessages = req.flash();
+	next();
 });
 //app.use(expressValidator());
 
 app.use("/", router);
 
 const server = app.listen(app.get("port"), () => {console.log(`Server running at http://localhost:${app.get("port")}`);
-}),
-io = require("socket.io")(server);
+	}),
+	io = require("socket.io")(server);
 require("./controllers/chatController")(io);
