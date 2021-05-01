c21d5e57d94c962e4372288f9ef493e671c024d2
iaxat <55215422+iaxat@users.noreply.github.com>
2021-05-01 02:33:21 -0600

    up

--- final_project/main.js
+++ final_project/main.js
@@ -20,6 +20,7 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }
 );
 
+
 const db = mongoose.connection;
 
 db.once("open", () => {
